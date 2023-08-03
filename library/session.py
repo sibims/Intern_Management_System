5701463baf2ca4aae7cf4c5a5bf6e12ab27e7c4a
sibims <mssibi047@gmail.com>
2023-08-03 09:20:36 +0530

    v1.0

---
+++ library/session.py
@@ -0,0 +1,25 @@
+from db.db_connector import get_db_connection
+
+from tabulate import tabulate
+
+
+class ScheduleManager:
+
+    def __init__(self):
+        self.db_connection = get_db_connection()
+
+    def view_schedule(self):
+        cursor = self.db_connection.cursor()
+
+        query = """SELECT session_id, session_title, handled_by, time, duration FROM schedules"""
+
+        cursor.execute(query)
+        sessions = cursor.fetchall()
+
+        if sessions:
+            headers = ["Session ID", "Title", "Handled By", "Time", "Duration"]
+            print(tabulate(sessions, headers=headers, tablefmt="grid"))
+        else:
+            print("No sessions found in the database.")
+        print("\n")
+        cursor.close()
---
+++ main.py
@@ -0,0 +1,6 @@
+
+from user.start import Start
+
+if __name__ == "__main__":
+    start = Start()
+    display_dashboard()
